0ec0a71ecbf450d3492d653ff45f95e49aa1752c
Rizardo-Jonas-BSCS32E1 <75775925+Rizardo-Jonas-BSCS32E1@users.noreply.github.com>
2024-05-30 21:13:01 +0800

    Completed

---
+++ PREFINALS_AUTH_Rizardo_Jonas/ProtectedApi/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    [Authorize]
+    public IActionResult Get()
+    {
+        var userInfo = new
+        {
+            Name = "Jonas Rizardo",
+            Section = "CS",
+            Course = "Computer Science",
+            FunFacts = new[]
+            {
+                "I love coding.",
+                "I am a coffee enthusiast.",
+                "I enjoy playing basketball.",
+                "I have a dog named Spark.",
+                "I am a movie buff.",
+                "I love watching anime.",
+                "I love one piece",
+                "I enjoy playing computer games",
+                "I love my girlfriend so much",
+                "I enjoy traveling."
+            }
+        };
+
+        return Ok(userInfo);
+    }
+}
